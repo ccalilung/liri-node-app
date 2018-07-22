f85dcd859ba366dfe5fe3857eb8315b0d596e883
ccalilung <brobeans289@gmail.com>
2018-07-21 23:33:35 -0400

    added log.txt, added tweet functionality, added README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # liri-node-app
+
+This project is the Language Interpretation and Recognition Interface. It uses command line arguments to call various APIs. To get started, clone the master and install the Node packages listed below in your project's root directory. You will also need a Spotify, Twitter, and OMDB account with API keys for each.
+
+CLI command #1 - "node liri.js my-tweets" - With a properly setup API key and call with Twitter, this CLI command prints the last 20 tweets and also logs them to log.txt located in the root file. This uses the Twitter npm package.
+
+CLI command #2 - "node liri.js spotify-this-song <song name here>" - With a properly setup API key and call with Spotify, this CLI command prints the artist, song name, preview URL, and album of a song searched. If no results come back in the API call, the call is made again where the output is for the song "The Sign by Ace of Base." The results also get logged to log.txt This uses the node-spotify-api npm package. 
+
+CLI command #3 - "node liri.js movie-this <movie title here>" - With a properly setup OMDB API key and call using the request npm package, this CLI command prints the title, year, IMDB rating, rottentomatoes rating, country, language, plot, actors of the movie. The results also get logged to log.txt.
+
+CLI command #4 - "node liri.js do-what-it-says" - This will call one of the 3 previous commands and run it. If spotify, the song searched is the Backstreet Boys "I want it that way." If OMDB search is called, the movie "Good Will Hunting" is searched.
+
+CLI command #5 - "node liri.js tweet <insert tweet here> - With a properly setup Twitter API key and call, this command will allow you to tweet from the CLI. The output is the tweet and that is logged to log.txt.
+
+The following Node packages are used: 
+[Twitter](https://www.npmjs.com/package/twitter)
+[Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+[Request](https://www.npmjs.com/package/request)
+[DotEnv](https://www.npmjs.com/package/dotenv)
+
+Also used are the Node File System and OMDB API (http://www.omdbapi.com).
+
+
--- liri.js
+++ liri.js
@@ -1,59 +1,70 @@
+//variables and required packages/files
 require("dotenv").config();
-
 var keys = require("./keys")
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api')
 var omdb = require('request')
 var fs = require('fs')
-
-
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+
+//run this function for the "my-tweets" argument
 function showTweets() {
-
-
   client.get('statuses/user_timeline', 20, function (error, tweets, response) {
-    if (!error) {
-
+    if (error) {
+      throw "error"
     }
-
     tweets.forEach(tweeter => {
-      console.log(tweeter.text + " " + tweeter.created_at)
+      var theTweet = tweeter.text + " " + tweeter.created_at
+      //log to log.txt file
+      fs.appendFile("log.txt",theTweet,function(err){
+        console.log(theTweet)
+      })
     })
   })
 }
 
+//run this function for the "spotify-this-song" argument
 function theSpotify() {
   var searchField = process.argv[3]
-  var check = 0
+
   function searchSpotify(searchField) {
     spotify.search({
       type: 'track',
       query: searchField
     }, function (err, data) {
       if (data === null) {
+        //if no data comes back from the search, run the search again with "The Sign Ace of Base" as the search term
         searchField = "The Sign Ace of Base"
         searchSpotify(searchField)
       }
-      else{
-      console.log("Artist(s): " + data.tracks.items[0].artists[0].name)
-      console.log("Song Name: " + data.tracks.items[0].name)
-      if (data.tracks.items[0].preview_url === null) {
-        console.log("No Preview URL available")
-      } else {
-        console.log(data.tracks.items[0].preview_url)
+      //if data comes back for the search, log in CLI the artist(s), song name, preview URL, and album
+      else {
+        var artist = "Artist(s): " + data.tracks.items[0].artists[0].name
+        var songName = "Song Name: " + data.tracks.items[0].name
+        if (data.tracks.items[0].preview_url === null) {
+          var previewURL = "No Preview URL available"
+        } else {
+          var previewURL = 'Preview URL: ' + data.tracks.items[0].preview_url
+        }
+        var album = "Album: " + data.tracks.items[0].album.name
+        //log output to log.txt
+        fs.appendFile("log.txt", artist + " " + songName + " " + previewURL + " " + album, function (err) {
+          console.log(artist)
+          console.log(songName)
+          console.log(previewURL)
+          console.log(album)
+        })
+
+
+
       }
-      console.log("Album: " + data.tracks.items[0].album.name)}
-
-
     });
   }
-
   searchSpotify(searchField);
-  
 }
-
+//Run this function for the "movie-this" argument
 function omdbQuery(search) {
   var search = process.argv[3];
   var searchSplit = search.split(" ")
@@ -61,34 +72,47 @@ function omdbQuery(search) {
 
   function omdbCall(searchTerm) {
     omdb('http://www.omdbapi.com/?t=' + searchTerm + '&y=&plot=short&apikey=25914f82', function (error, response, body) {
-
       if (!error && response.statusCode === 200) {
+        //if the search term returns nothing, response will be false. if false, rerun the search with Mr Nobody as the search parameter
         if (JSON.parse(body).Response === "False") {
-          console.log("Wrong!")
           searchTerm = "Mr+Nobody"
           omdbCall(searchTerm);
-
-        } else {
-          console.log("The movie's title: " + JSON.parse(body).Title)
-          console.log("Year: " + JSON.parse(body).Year)
-          console.log("The movie's IMDB Rating: " + JSON.parse(body).imdbRating)
-          let rating = JSON.parse(body).Ratings
+        }
+        //if the search term returns a movie, print title, year, movie, rating, country, language, actors, and plot
+        else {
+          var title = "The movie's title: " + JSON.parse(body).Title
+          var year = "Year: " + JSON.parse(body).Year
+          var imdbRating = "IMDB Rating: " + JSON.parse(body).imdbRating
+          var rating = (JSON.parse(body).Ratings)
           for (i = 0; i < rating.length; i++) {
             if (rating[i].Source === "Rotten Tomatoes") {
-              console.log("Rotten Tomatoes Rating: " + rating[i].Value)
+              var rottenTomatoes = "Rotten Tomatoes Rating: " + rating[i].Value
             }
           }
-          console.log("Country: " + JSON.parse(body).Country)
-          console.log("Language: " + JSON.parse(body).Language)
-          console.log("Actors: " + JSON.parse(body).Actors)
-          console.log("Plot: " + JSON.parse(body).Plot)
+          var country = "Country: " + JSON.parse(body).Country
+          var language = "Language: " + JSON.parse(body).Language
+          var actors = "Actors: " + JSON.parse(body).Actors
+          var plot = "Plot: " + JSON.parse(body).Plot
+          //log to log.txt
+          fs.appendFile("log.txt", title + " " + year + " " + imdbRating + " " + rottenTomatoes + " " + country + " " + language + " " + actors + " " + plot, function (err) {
+            if (err) {
+              return console.log(err);
+            }
+            console.log(title)
+            console.log(year)
+            console.log(imdbRating)
+            console.log(rottenTomatoes)
+            console.log(country)
+            console.log(language)
+            console.log(actors)
+            console.log(plot)
+          })
 
         }
       }
     })
   }
   omdbCall(searchTerm);
-
 }
 
 function tweet() {
@@ -96,29 +120,31 @@ function tweet() {
     status: process.argv[3]
   }, function (error, tweet, response) {
     if (!error) {;
-      console.log("Tweeted: " + tweet)
+    var theTweet = "Tweeted: " + tweet.text;
+    fs.appendFile("log.txt",theTweet,function(err){
+      if (err){
+        throw "error"
+      }
+      console.log(theTweet)
+    })
     }
   });
 }
 
 
-
+//CLI inputs to run the functions
 if (process.argv[2] === "movie-this") {
   omdbQuery();
 }
-
 if (process.argv[2] === "my-tweets") {
   showTweets();
 }
-
 if (process.argv[2] === "spotify-this-song") {
   theSpotify();
 }
-
 if (process.argv[2] === "tweet") {
   tweet();
 }
-
 if (process.argv[2] === "do-what-it-says") {
   fs.readFile("random.txt", "utf8", function (error, data) {
     var dataArr = data.split(",");
@@ -133,6 +159,8 @@ if (process.argv[2] === "do-what-it-says") {
       process.argv[3] = newArr[1]
       omdbQuery();
     }
+    if (rand === 1) {
+      showTweets();
+    }
   })
-
 }
